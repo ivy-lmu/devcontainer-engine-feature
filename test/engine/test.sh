3246ccaa43a07a069104041dbc73a25657e1c4ed
Louis <louis.mueller@axonivy.com>
2023-12-29 14:54:49 +0100

    wip

--- test/engine/test.sh
+++ test/engine/test.sh
@@ -4,6 +4,6 @@ set -e
 
 source dev-container-features-test-lib
 
-check "engine directory exist" cd /usr/lib/axonivy-engine/binxfx/
+check "engine directory exist" cd /usr/lib/axonivy-engine/bin/
 
 reportResults
