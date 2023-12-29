431fdca1f6e42feaf8b7b9e8c9c7c479b4c5ca55
Louis <louis.mueller@axonivy.com>
2023-12-29 12:14:55 +0100

    add download url and engine home options

--- src/engine/install.sh
+++ src/engine/install.sh
@@ -1,33 +1,24 @@
 #!/bin/sh
 set -e
 
-IVY_ENGINE_DOWNLOAD_URL="https://dev.axonivy.com/permalink/dev/axonivy-engine-slim.zip"
-IVY_HOME="/usr/lib/axonivy-engine"
-
-echo "Activating feature 'hello'"
-
-echo "The effective dev container remoteUser is '$_REMOTE_USER'"
-echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
-
-echo "The effective dev container containerUser is '$_CONTAINER_USER'"
-echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
-
-# Checks if packages are installed and installs them if not
 check_packages() {
-    if ! dpkg -s "$@" > /dev/null 2>&1; then
-        apt_get_update
-        apt-get -y install --no-install-recommends "$@"
-    fi
+  if ! dpkg -s "$@" > /dev/null 2>&1; then
+    apt_get_update
+    apt-get -y install --no-install-recommends "$@"
+  fi
 }
 
+echo "Installing wget if needed."
 check_packages wget unzip
 
-wget ${IVY_ENGINE_DOWNLOAD_URL} -O /tmp/ivy.zip --no-verbose
-unzip /tmp/ivy.zip -d ${IVY_HOME}
+echo "Downloading Axon Ivy Engine from '$DOWNLOADURL'"
+wget ${DOWNLOADURL} -O /tmp/ivy.zip --no-verbose
+
+echo "Unziping Axon Ivy Engine to '$ENGINEHOME'"
+unzip /tmp/ivy.zip -d ${ENGINEHOME}
 rm -f /tmp/ivy.zip
-    
-    
-mkdir ${IVY_HOME}/applications
-mkdir ${IVY_HOME}/configuration/applications && \
-chown -R vscode:0 ${IVY_HOME} && \
-chmod -R g=u ${IVY_HOME}
+
+mkdir ${ENGINEHOME}/applications
+mkdir ${ENGINEHOME}/configuration/applications && \
+chown -R ${_REMOTE_USER}:0 ${ENGINEHOME} && \
+chmod -R g=u ${ENGINEHOME}
